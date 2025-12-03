58d3802acf721d96f6125785e1fa6b7dd6f91495
Gabriel Pedro <g.pedro0972@gmail.com>
2025-12-03 11:10:48 -0300

    Ajustando Texto de experiencias e participações

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,13 +7,20 @@ function mostrarConteudo(secao) {
 
   const textos = {
     sobveu: `<h2>SET 2025 - DEZ 2025</h2>
-    <h6>Projeto SobVéu - Site E-commerce para venda de vestidos de noiva</h6>
-    <p>fgffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+<h6>Projeto SobVéu - Site E-commerce para venda de vestidos de noiva</h6>
+<p> > Estruturei rotas de API para o CRUD de produtos.</p>
+<br>
+<p> > Criei toda a parte de processamento de compras — rotas de Carrinho e Checkout.</p>
+<br>
+<p> > Desenvolvi as rotas de Frete e o redirecionamento para as telas de Checkout e painel de usuário.</p>
+<br>
+<p> > Auxiliei os devs Front-End na correção do bug de cobrança exagerada nos pedidos.</p>
+
     `,
 
     calculadora: `<h2>DEZ 2025 - FEV 2026</h2>
     <h6>Projeto Calculadora</h6>
-    <p>fgffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+    <p></p>
     `,
   };
 
