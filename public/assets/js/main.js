cae8d1bf5796946fb6530c5efaa3175463c3f120
Gabriel Pedro <g.pedro0972@gmail.com>
2025-12-08 09:31:20 -0300

    Atualizando Experiencias

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,7 +7,7 @@ function mostrarConteudo(secao) {
 
   const textos = {
     sobveu: `<h2>SET 2025 - DEZ 2025</h2>
-<h6>Projeto SobVéu - Site E-commerce para venda de vestidos de noiva</h6>
+<h5>Projeto SobVéu - Site E-commerce para venda de vestidos de noiva</h5>
 <p> > Estruturei rotas de API para o CRUD de produtos.</p>
 <br>
 <p> > Criei toda a parte de processamento de compras — rotas de Carrinho e Checkout.</p>
@@ -19,9 +19,26 @@ function mostrarConteudo(secao) {
     `,
 
     calculadora: `<h2>DEZ 2025 - FEV 2026</h2>
-    <h6>Projeto Calculadora</h6>
-    <p></p>
+    <h5>Projeto Calculadora</h5>
+    <p> > Calculator: Criei a classe pai Calculator, 
+    que é um modelo de uma calculadora real, 
+    que efetua calculos com dois termos x e y. </p>
+    <br>
+    <p> > Controller: Criei o controtroller para ser a ponte,
+    entre a calculadora e a Interface gráfica.</p>
+    <br>
+    <p> > UI: Classe CalculatorUI, que é responsável por criar e
+    gerenciar a interface gráfica da calculadora.</p>
+
+    <br>
+    <p> > Main: Ponto de partida do programa. 
+    Junta todos os arquivos e executa o programa</p>
+
+    <br>
+    <p>(...) Mais em Breve!</p>
     `,
+
+    
   };
 
   conteudo.innerHTML = textos[secao];
